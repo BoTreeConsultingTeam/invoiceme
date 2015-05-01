ec600365714e7d395f42d137f1695777363b1274
SubrahmanyamKonduri-BTC <subrahmanyam.konduri@botreeconsulting.com>
2015-05-01 10:51:12 +0530

    devise related changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,26 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  ## Associations
   belongs_to :company
-  
   has_many :invoices, through: :company
   has_many :clients, through: :company
-  
+
+  ## Callbacks
+  after_create :assign_role_and_admin_id
+
+  def assign_role_and_admin_id
+    if (self.role.nil? and self.admin_id.nil?)
+      user = User.find_by(:role => "Admin", :admin_id => self.id)
+      if user.nil?
+        self.update_column(:role, "Admin")
+        self.update_column(:admin_id, self.id)
+      end
+    end
+  end
+
 end
--- db/migrate/20150303134324_add_columns_to_users.rb
+++ db/migrate/20150303134324_add_columns_to_users.rb
@@ -3,5 +3,7 @@ class AddColumnsToUsers < ActiveRecord::Migration
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :company_id, :integer
+    add_column :users, :admin_id, :integer
+    add_column :users, :role, :string
   end
 end
