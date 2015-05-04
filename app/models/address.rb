a0f0c9349da75805b3bc6e416630a00ba6672d78
PragatiDoshi-BTC <pragati.doshi@botreeconsulting.com>
2015-05-04 18:59:18 +0530

    client crud with country, currency and pincode gems

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ActiveRecord::Base
   belongs_to :client
+
+  validates_presence_of :street_1, :city, :state, :country_code, :pincode
+  validates :pincode, zipcode: { country_code_attribute: :country_code }
+
+  def country_name
+    country = ISO3166::Country[country_code]
+    if country.present?
+      return country.translations[I18n.locale.to_s] || country.name
+    else
+      return ''
+    end
+  end
+
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,10 @@
 class Client < ActiveRecord::Base
 
-  belongs_to :currency
   belongs_to :company
   
   has_one :address
   has_many :contact_details
   has_many :invoices
   
+  validates_presence_of :name, :currency_code, :company_id
 end
