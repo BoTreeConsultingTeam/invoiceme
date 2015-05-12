63223028d47a9ca2f4b80b8ed1806511fa62cfa5
PragatiDoshi-BTC <pragati.doshi@botreeconsulting.com>
2015-05-12 10:31:30 +0530

    changes according to the comment

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,11 @@
 module ClientsHelper
+
+  def name(contact_detail)
+    contact_detail.first_name+' '+contact_detail.last_name
+  end
+
+  def location(client)
+    client.address_city.to_s+' '+country_name(client.address_country_code).to_s
+  end
+
 end
