073633e5f87886ca93f2f2fa9e5c444fce07306f
ParthivPatel-BTC <parthiv.patel@botreeconsulting.com>
2015-05-12 15:14:16 +0530

    Removed space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,5 +41,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :admin_id, :role) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :current_password) }
   end
-
 end
