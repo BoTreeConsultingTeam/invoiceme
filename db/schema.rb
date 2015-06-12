d5351dbf380db677e511a7c1e8c9e77b9dac8f19
PragatiDoshi-BTC <pragati.doshi@botreeconsulting.com>
2015-06-12 20:00:22 +0530

    # 54 - sr no in list and invoice history

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506093406) do
+ActiveRecord::Schema.define(version: 20150605060110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_1"
@@ -22,10 +39,11 @@ ActiveRecord::Schema.define(version: 20150506093406) do
     t.string   "city"
     t.string   "state"
     t.integer  "pincode"
-    t.integer  "client_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "country_code"
+    t.integer  "addressdetail_id"
+    t.string   "addressdetail_type"
   end
 
   create_table "clients", force: :cascade do |t|
@@ -69,19 +87,21 @@ ActiveRecord::Schema.define(version: 20150506093406) do
 
   create_table "invoices", force: :cascade do |t|
     t.integer  "client_id"
-    t.string   "invoice_number"
-    t.datetime "date_of_issue"
     t.string   "po_number"
     t.float    "discount"
     t.float    "subtotal"
     t.float    "total"
-    t.datetime "paid_to_date"
     t.float    "balance"
     t.string   "notes"
     t.text     "terms"
     t.integer  "payment_days"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.date     "date_of_issue"
+    t.date     "paid_to_date"
+    t.integer  "invoice_number"
+    t.string   "currency_code"
+    t.integer  "status",         default: 0, null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -92,14 +112,37 @@ ActiveRecord::Schema.define(version: 20150506093406) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "line_item_taxes", force: :cascade do |t|
+    t.integer  "tax_id"
+    t.integer  "line_item_id"
+    t.float    "rate"
+    t.float    "amount"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "line_items", force: :cascade do |t|
     t.integer  "item_id"
     t.integer  "invoice_id"
-    t.integer  "line_total"
     t.float    "price"
     t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.float    "line_total"
+    t.float    "discount"
+    t.integer  "tax1"
+    t.integer  "tax2"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer  "invoice_id"
+    t.float    "payment_amount"
+    t.integer  "payment_method"
+    t.date     "date_of_payment"
+    t.text     "notes"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "taxes", force: :cascade do |t|
@@ -108,6 +151,7 @@ ActiveRecord::Schema.define(version: 20150506093406) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "parent_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -130,7 +174,6 @@ ActiveRecord::Schema.define(version: 20150506093406) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "company_id"
-    t.integer  "admin_id"
     t.integer  "role"
   end
 
